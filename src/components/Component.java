39dcfc0139b3ab22af34b2f55dcf0015567ffbbb
Xavi <Xavi@DESKTOP-QHE21O9>
2018-12-03 18:14:01 +0100

    Component done

---
+++ src/components/Component.java
@@ -0,0 +1,46 @@
+package components;
+
+import java.text.ParseException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xavi
+ */
+
+public interface Component {
+    
+    Scanner DADES= new Scanner(System.in);
+    
+    public abstract void mostrarComponent();
+    public void modificarComponent() throws ParseException;
+    
+    default Object demanarDades(String peticio, int tipus){
+        
+        int enter; String token, cadena; double decimal;
+        
+        System.out.println(peticio);
+        
+        switch (tipus) {
+            
+                case 1:
+                    enter = DADES.nextInt();
+                    DADES.nextLine();
+                    return enter;
+                case 2:
+                    token = DADES.next();
+                    DADES.nextLine();
+                    return token;
+                case 3:
+                    cadena = DADES.nextLine();
+                    return cadena;
+                case 4:
+                    decimal = DADES.nextDouble();
+                    DADES.nextLine();
+                    return decimal;
+            }
+        
+        return null;
+    }
+    
+}
